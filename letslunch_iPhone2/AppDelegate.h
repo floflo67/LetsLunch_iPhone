348bb5113ecf7ddfefb91084133602e8f5e8c1da
Florian Reiss <85802@supinfo.com>
2013-06-14 09:51:31 -0700

    Initial Commit

---
+++ letslunch_iPhone2/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  letslunch_iPhone2
+//
+//  Created by Florian Reiss on 14/06/13.
+//  Copyright (c) 2013 Florian Reiss. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
