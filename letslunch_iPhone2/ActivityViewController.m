54dd81a3b3fc016b6aeeb1b9d4491f733cbe8b9e
Florian Reiss <85802@supinfo.com>
2013-06-26 11:58:17 -0700

    Suppressing comments

--- letslunch_iPhone2/ActivityViewController.m
+++ letslunch_iPhone2/ActivityViewController.m
@@ -152,14 +152,7 @@ static ActivityViewController *sharedSingleton = nil;
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    // Navigation logic may go here. Create and push another view controller.
-    /*
-     <#DetailViewController#> *detailViewController = [[<#DetailViewController#> alloc] initWithNibName:@"<#Nib name#>" bundle:nil];
-     // ...
-     // Pass the selected object to the new view controller.
-     [self.navigationController pushViewController:detailViewController animated:YES];
-     [detailViewController release];
-     */
+    
 }
 
 @end
