c1369de2a204ae26e7f23dca349c3f8dff1b51bc
Florian Reiss <85802@supinfo.com>
2013-06-27 18:13:31 -0700

    Adding loginVC

---
+++ letslunch_iPhone2/LoginViewController.h
@@ -0,0 +1,17 @@
+//
+//  LoginViewController.h
+//  letslunch_iPhone2
+//
+//  Created by Florian Reiss on 27/06/13.
+//  Copyright (c) 2013 Florian Reiss. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@property (nonatomic, strong) NSArray *objects;
+@property (retain, nonatomic) IBOutlet UITableView *tableView;
+
+
+@end
---
+++ letslunch_iPhone2/LoginViewController.m
@@ -0,0 +1,79 @@
+//
+//  LoginViewController.m
+//  letslunch_iPhone2
+//
+//  Created by Florian Reiss on 27/06/13.
+//  Copyright (c) 2013 Florian Reiss. All rights reserved.
+//
+
+#import "LoginViewController.h"
+
+@interface LoginViewController ()
+
+@end
+
+@implementation LoginViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    if(!_objects) {
+        _objects = [[NSArray alloc] initWithObjects:@"Login", @"Password", nil];
+    }
+    NSLog(@"test");
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+}
+
+- (void)dealloc {
+    [_tableView release];
+    [super dealloc];
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [_objects count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"Cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    
+    if (cell == nil) {
+        cell = [[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:CellIdentifier] autorelease];
+    }
+    cell.backgroundColor = [UIColor clearColor];
+    cell.selectionStyle = UITableViewCellSelectionStyleNone;
+    cell.textLabel.text = [_objects[indexPath.row] description];
+    
+    return cell;
+}
+
+#pragma mark - Table view delegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    
+}
+
+@end
