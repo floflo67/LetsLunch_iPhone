3de97c1a6840ce8b8a7ec813735187def8ebfdc6
Florian Reiss <85802@supinfo.com>
2013-07-03 14:42:17 -0700

    Using activity class
    Using the activity class as a provider.

--- GetStaticLists.m
+++ GetStaticLists.m
@@ -17,16 +17,24 @@
 + (NSArray*)getListActivities
 {
     NSMutableArray* listActivities = [[[NSMutableArray alloc] init] autorelease];
-    //Messages *m = [[Messages alloc] initWithContent:@"mess" From:@"1" To:@"2" date:[NSDate new]];
-    //Contacts *c = [[Contacts alloc] initWithID:@"1" withName:@"name" withPictureURL:nil andLastMessage:nil];
-    //FSVenue *v = [[FSVenue alloc] init];
-    //BOOL b = YES;
-    //Activity *act1 = [[Activity alloc] initWithContact:nil venue:nil description:@"description" andIsCoffee:b];
+    Messages *m = [[Messages alloc] initWithDescription:@"mess" From:@"1" To:@"2" date:[NSDate new]];
+    Contacts *c = [[Contacts alloc] initWithID:@"1" withName:@"name" withPictureURL:nil andLastMessage:m];
+    FSVenue *v = [[FSVenue alloc] init];
     
-    [listActivities addObject:@"Activity1"];
+    v.name = @"The Grove";
+    v.venueId = @"4b7ef75cf964a520ee0c30e3";
+    v.location.address = @"690 Mission St";
+    NSNumber *num = [NSNumber numberWithInt:403];
+    v.location.distance = num;
+    v.distance = v.location.distance;
+    [v.location setCoordinate:CLLocationCoordinate2DMake([@"37.78653398485857" doubleValue], [@"-122.401921749115" doubleValue])];
+    
+    BOOL b = YES;
+    Activity *act1 = [[Activity alloc] initWithContact:c venue:v description:@"desc1" andIsCoffee:b];
+    
+    [listActivities addObject:act1];
     [listActivities addObject:@"Activity2"];
     [listActivities addObject:@"Activity3"];
-    
     
     return listActivities;
 }
