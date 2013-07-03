26eef4268d7c13c5c6ffe716d5587a45fde7238b
Florian Reiss <85802@supinfo.com>
2013-07-03 10:42:11 -0700

    Using detail
    Using the new detail view. Allowing selection on current table.

--- letslunch_iPhone2/ActivityViewController.m
+++ letslunch_iPhone2/ActivityViewController.m
@@ -8,6 +8,8 @@
 
 #import "ActivityViewController.h"
 #import "AppDelegate.h"
+#import "DetailProfileViewController.h"
+#import "AppDelegate.h"
 
 @interface ActivityViewController ()
 
@@ -89,6 +91,7 @@ static ActivityViewController *sharedSingleton = nil;
     }
     cell.backgroundColor = [UIColor clearColor];
     cell.selectionStyle = UITableViewCellSelectionStyleNone;
+    cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
     
     if([[_objects[indexPath.row] description] isEqualToString:@"NIL"]) {
         
@@ -108,50 +111,15 @@ static ActivityViewController *sharedSingleton = nil;
     return cell;
 }
 
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
-}
-*/
-
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    }   
-    else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
-{
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
-
 #pragma mark - Table view delegate
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    
+    if(indexPath.row > 0) {
+        DetailProfileViewController *detail = [[[DetailProfileViewController alloc] init] autorelease];
+        [((AppDelegate*)[UIApplication sharedApplication].delegate).viewController.navigationController pushViewController:detail animated:YES];
+        detail = nil;
+    }
 }
 
 @end
