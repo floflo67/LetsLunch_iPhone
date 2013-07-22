ad3dd39635238976bc1ff57289d19d99f1086bd0
Florian Reiss <85802@supinfo.com>
2013-07-22 15:08:57 -0700

    Changing DetailProfileViewController
    Adding a tableView. Setting content of tableView.

--- letslunch_iPhone2/ViewControllers/Detail/DetailProfileViewController.h
+++ letslunch_iPhone2/ViewControllers/Detail/DetailProfileViewController.h
@@ -2,14 +2,21 @@
 //  DetailProfileViewController.h
 //  letslunch_iPhone2
 //
-//  Created by Florian Reiss on 03/07/13.
+//  Created by Florian Reiss on 22/07/13.
 //  Copyright (c) 2013 Florian Reiss. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
+#import "ProfileDetailsRequest.h"
 
-@interface DetailProfileViewController : UITableViewController
+@interface DetailProfileViewController : UIViewController <UITableViewDataSource, UITableViewDelegate> {
+    ProfileDetailsRequest *profileDetailRequest;
+}
 
-@property (nonatomic, strong) NSMutableArray *objects;
+@property (nonatomic, strong) NSMutableDictionary* objects;
+@property (nonatomic, retain) IBOutlet UITableView *tableView;
+@property (nonatomic, strong) ProfileDetailsRequest *profileDetailRequest;
+
+-(id)initWithContactID:(NSString*)contactID;
 
 @end
--- letslunch_iPhone2/ViewControllers/Detail/DetailProfileViewController.m
+++ letslunch_iPhone2/ViewControllers/Detail/DetailProfileViewController.m
@@ -2,23 +2,32 @@
 //  DetailProfileViewController.m
 //  letslunch_iPhone2
 //
-//  Created by Florian Reiss on 03/07/13.
+//  Created by Florian Reiss on 22/07/13.
 //  Copyright (c) 2013 Florian Reiss. All rights reserved.
 //
 
 #import "DetailProfileViewController.h"
+#import "ProfileDetailsRequest.h"
 
 @interface DetailProfileViewController ()
 
 @end
 
 @implementation DetailProfileViewController
+@synthesize profileDetailRequest;
 
-- (id)initWithStyle:(UITableViewStyle)style
+- (id)initWithContactID:(NSString*)contactID
 {
-    self = [super initWithStyle:style];
-    if (self) {
-        
+    self = [super init];
+    if(self) {
+        if(!_objects) {
+            if(!profileDetailRequest)
+                profileDetailRequest = [[ProfileDetailsRequest alloc] init];
+
+            _objects = [[[NSMutableDictionary alloc] initWithDictionary:[profileDetailRequest getProfileWithToken:[AppDelegate getObjectFromKeychainForKey:kSecAttrAccount] andID:contactID]] retain];
+            
+            profileDetailRequest = nil;
+        }
     }
     return self;
 }
@@ -26,26 +35,36 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    if(!self.objects) {
-        self.objects = [[[NSMutableArray alloc] initWithObjects:@"Name", @"Description", @"Other", nil] autorelease];
-    }
 }
 
-- (void)didReceiveMemoryWarning
+- (void)dealloc
 {
-    [super didReceiveMemoryWarning];
+    [_objects release];
+    [_tableView release];
+    [super dealloc];
 }
 
 #pragma mark - Table view data source
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
-    return 1;
+    return [_objects count];
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return [_objects count];
+    NSArray *keys = [_objects allValues];
+    NSArray *subsec = keys[section];
+    if([subsec count] > 0)
+        return [subsec count];
+    else
+        return 1;
+}
+
+-(NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section
+{
+    NSArray *keys = [_objects allKeys];
+    return keys[section];
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
@@ -53,18 +72,30 @@
     static NSString *CellIdentifier = @"Cell";
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
     if (cell == nil) {
-        cell = [[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier] autorelease];
+        cell = [[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:CellIdentifier] autorelease];
+    }
+    cell.selectionStyle = UITableViewCellSelectionStyleNone;
+    
+    NSArray *object = [_objects allValues];
+    
+    if([[object[indexPath.section] class] isSubclassOfClass:[NSDictionary class]]) {
+        NSArray *keys = [object[indexPath.section] allKeys];
+        NSArray *value = [object[indexPath.section] allValues];
+        cell.textLabel.text = [keys[indexPath.row] description];
+        cell.detailTextLabel.text = [value[indexPath.row] description];        
+    }
+    else if ([[object[indexPath.section] class] isSubclassOfClass:[NSArray class]]) {
+        if([object[indexPath.section] count] > 0) {
+            cell.textLabel.text = [object[indexPath.section][indexPath.row] description];            
+        }
+        else {
+            cell.textLabel.text = @"None listed";
+            cell.detailTextLabel.text = @"";
+        }
     }
     
-    cell.textLabel.text = self.objects[indexPath.row];
     
     return cell;
-}
-
-- (void)dealloc
-{
-    [self.objects release];
-    [super dealloc];
 }
 
 @end
