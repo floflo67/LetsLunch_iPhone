6eb136ef42e8c9f638d2d090ac499fcd03226d6d
Florian Reiss <85802@supinfo.com>
2013-07-31 17:04:13 -0700

    Enable share on Facebook

--- letslunch_iPhone2/ViewControllers/Share/ShareViewController.m
+++ letslunch_iPhone2/ViewControllers/Share/ShareViewController.m
@@ -7,9 +7,13 @@
 //
 
 #import "ShareViewController.h"
+#import <Social/Social.h>
+#import <Accounts/Accounts.h>
 
 @interface ShareViewController ()
-@property (weak, nonatomic) IBOutlet UIButton *buttonClose;
+@property (nonatomic, strong) SLComposeViewController *mySLComposerSheet;
+@property (weak, nonatomic) IBOutlet UIButton *facebookButton;
+@property (nonatomic) BOOL shareFacebook;
 @end
 
 @implementation ShareViewController
@@ -34,12 +38,69 @@ static ShareViewController *sharedSingleton = nil;
         sharedSingleton = nil;
 }
 
-- (void)viewDidLoad
+#pragma mark - Social sharing
+
+- (void)shareOnFacebook
 {
-    [super viewDidLoad];
-    [self.buttonClose addTarget:((AppDelegate*)[[UIApplication sharedApplication] delegate]).viewController
-                         action:@selector(closeView:)
-               forControlEvents:UIControlEventTouchDown];
+    __weak typeof(self) weakSelf = self;
+    
+    self.mySLComposerSheet = [SLComposeViewController composeViewControllerForServiceType:SLServiceTypeFacebook]; //Tell him with what social plattform to use it, e.g. facebook or twitter
+    [self.mySLComposerSheet setInitialText:@"Test"/*[NSString stringWithFormat:@"Test", self.mySLComposerSheet.serviceType]*/]; //the message you want to post
+    //for more instance methodes, go here:https://developer.apple.com/library/ios/#documentation/NetworkingInternet/Reference/SLComposeViewController_Class/Reference/Reference.html#//apple_ref/doc/uid/TP40012205
+    [self presentViewController:self.mySLComposerSheet animated:YES completion:nil];
+    
+    
+    [self.mySLComposerSheet setCompletionHandler:^(SLComposeViewControllerResult result) {
+        BOOL success = NO;
+        NSString *output;
+        switch (result) {
+            case SLComposeViewControllerResultCancelled:
+                output = @"Action Cancelled";
+                break;
+            case SLComposeViewControllerResultDone:
+                output = @"Post Successfull";
+                success = YES;
+                break;
+            default:
+                break;
+        }
+        
+        //check if everythink worked properly. Give out a message on the state.
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Facebook" message:output delegate:nil cancelButtonTitle:@"Ok" otherButtonTitles:nil];
+        [alert show];
+        
+        if(success) {
+            weakSelf.facebookButton.alpha = 0.4;
+            weakSelf.facebookButton.enabled = NO;
+        }
+    }];
+}
+
+#pragma mark - Button events
+
+- (IBAction)facebookButton:(UIButton*)sender
+{
+    if(!self.facebookButton.selected)
+        [self shareOnFacebook];
+    
+    self.facebookButton.selected = !self.facebookButton.selected;
+}
+
+- (IBAction)closeButton:(id)sender
+{
+    if(self.shareFacebook)
+       [self shareOnFacebook];
+    
+    [((AppDelegate*)[[UIApplication sharedApplication] delegate]).viewController closeView];
+}
+
+#pragma mark - getter and setter
+
+- (SLComposeViewController*)mySLComposerSheet
+{
+    if(!_mySLComposerSheet)
+        _mySLComposerSheet = [[SLComposeViewController alloc] init];
+    return _mySLComposerSheet;
 }
 
 @end
