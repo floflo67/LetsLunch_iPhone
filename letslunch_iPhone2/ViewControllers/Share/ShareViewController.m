5eba2f602518dadc2b11a5a1ab0adf2d6ada4294
Florian Reiss <85802@supinfo.com>
2013-08-01 15:00:28 -0700

    Using new files
    Using new files to post on facebook. Post when clicking on promote.

--- letslunch_iPhone2/ViewControllers/Share/ShareViewController.m
+++ letslunch_iPhone2/ViewControllers/Share/ShareViewController.m
@@ -9,11 +9,14 @@
 #import "ShareViewController.h"
 #import <Social/Social.h>
 #import <Accounts/Accounts.h>
+#import <FacebookSDK/FacebookSDK.h>
+#import "FacebookShare.h"
 
 @interface ShareViewController ()
 @property (nonatomic, strong) SLComposeViewController *mySLComposerSheet;
 @property (weak, nonatomic) IBOutlet UIButton *facebookButton;
 @property (weak, nonatomic) IBOutlet UIButton *twitterButton;
+@property (nonatomic) BOOL mustShareOnFacebook;
 @end
 
 @implementation ShareViewController
@@ -42,39 +45,10 @@ static ShareViewController *sharedSingleton = nil;
 
 - (void)shareOnFacebook
 {
-    __weak typeof(self) weakSelf = self;
-    
-    self.mySLComposerSheet = [SLComposeViewController composeViewControllerForServiceType:SLServiceTypeFacebook]; //Tell him with what social plattform to use it, e.g. facebook or twitter
-    [self.mySLComposerSheet setInitialText:@"Test Facebook"/*[NSString stringWithFormat:@"Test", self.mySLComposerSheet.serviceType]*/]; //the message you want to post
-    //for more instance methodes, go here:https://developer.apple.com/library/ios/#documentation/NetworkingInternet/Reference/SLComposeViewController_Class/Reference/Reference.html#//apple_ref/doc/uid/TP40012205
-    [self presentViewController:self.mySLComposerSheet animated:YES completion:nil];
-    
-    
-    [self.mySLComposerSheet setCompletionHandler:^(SLComposeViewControllerResult result) {
-        BOOL success = NO;
-        NSString *output;
-        switch (result) {
-            case SLComposeViewControllerResultCancelled:
-                output = @"Action Cancelled";
-                break;
-            case SLComposeViewControllerResultDone:
-                output = @"Post Successfull";
-                success = YES;
-                break;
-            default:
-                break;
-        }
-        
-        if(success) {
-            weakSelf.facebookButton.alpha = 0.4;
-            weakSelf.facebookButton.enabled = NO;
-        }
-        
-        if(weakSelf.mySLComposerSheet) {
-            [weakSelf.mySLComposerSheet dismissViewControllerAnimated:YES completion:nil];
-            weakSelf.mySLComposerSheet = nil;
-        }
-    }];
+    FacebookShare *facebookShare = [[FacebookShare alloc] init];
+    [facebookShare postToFacebook:@"Let's Lunch - iPhone" productLink:@"www.letslunch.com" productImageUrl:@"http://farm6.static.flickr.com/5065/5681696034_e9f67e2181.jpg"];
+    self.facebookButton.enabled = NO;
+    self.facebookButton.alpha = 0.3;
 }
 
 - (void)shareOnTwitter
@@ -118,11 +92,9 @@ static ShareViewController *sharedSingleton = nil;
 #pragma mark - Button events
 
 - (IBAction)facebookButton:(UIButton*)sender
-{
-    if(!self.facebookButton.selected)
-        [self shareOnFacebook];
-    
+{    
     self.facebookButton.selected = !self.facebookButton.selected;
+    self.mustShareOnFacebook = self.facebookButton.selected;
 }
 
 - (IBAction)twitterButton:(UIButton*)sender
@@ -134,8 +106,14 @@ static ShareViewController *sharedSingleton = nil;
 }
 
 - (IBAction)closeButton:(id)sender
-{    
+{
     [((AppDelegate*)[[UIApplication sharedApplication] delegate]).viewController closeView];
+}
+
+- (IBAction)promoteButton:(UIButton *)sender
+{
+    if(self.mustShareOnFacebook)
+        [self shareOnFacebook];
 }
 
 #pragma mark - getter and setter
