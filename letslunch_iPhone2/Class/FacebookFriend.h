206aac3cc2aaf5ba22760dd8ed435afb719fe5dc
Florian Reiss <85802@supinfo.com>
2013-08-02 12:59:30 -0700

    Creating class for FacebookFriends

---
+++ letslunch_iPhone2/Class/FacebookFriend.h
@@ -0,0 +1,19 @@
+//
+//  FacebookFriend.h
+//  letslunch_iPhone2
+//
+//  Created by Florian Reiss on 02/08/13.
+//  Copyright (c) 2013 Florian Reiss. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FacebookFriend : NSObject
+
+@property (nonatomic, strong, readonly) NSString *userID;
+@property (nonatomic, strong, readonly) NSString *firstName;
+@property (nonatomic, strong, readonly) NSString *lastName;
+
+-(id)initWithID:(NSString*)userID firstname:(NSString *)firstname andLastname:(NSString *)lastname;
+
+@end
---
+++ letslunch_iPhone2/Class/FacebookFriend.m
@@ -0,0 +1,35 @@
+//
+//  FacebookFriend.m
+//  letslunch_iPhone2
+//
+//  Created by Florian Reiss on 02/08/13.
+//  Copyright (c) 2013 Florian Reiss. All rights reserved.
+//
+
+#import "FacebookFriend.h"
+
+@interface FacebookFriend()
+@property (nonatomic, strong, readwrite) NSString *userID;
+@property (nonatomic, strong, readwrite) NSString *firstName;
+@property (nonatomic, strong, readwrite) NSString *lastName;
+@end
+
+@implementation FacebookFriend
+
+- (id)initWithID:(NSString*)userID firstname:(NSString *)firstname andLastname:(NSString *)lastname
+{
+    self = [super init];
+    if(self) {
+        self.userID = userID;
+        self.firstName = firstname;
+        self.lastName = lastname;
+    }
+    return self;
+}
+
+- (NSString*)description
+{
+    return [NSString stringWithFormat:@"id: %@, first name: %@, last name: %@", self.userID, self.firstName, self.lastName];
+}
+
+@end
