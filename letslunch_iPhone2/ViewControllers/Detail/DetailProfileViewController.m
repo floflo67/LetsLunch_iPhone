03a1b84553d2a9c61a406d77c2b49f217d8a2297
Florian Reiss <85802@supinfo.com>
2013-08-08 11:14:28 -0700

    Change in API
    Adding mediaLinks, testimonials, and wishList.

--- letslunch_iPhone2/ViewControllers/Detail/DetailProfileViewController.m
+++ letslunch_iPhone2/ViewControllers/Detail/DetailProfileViewController.m
@@ -12,8 +12,9 @@
 
 @interface DetailProfileViewController ()
 @property (nonatomic, weak) IBOutlet UITableView *tableView;
-@property (nonatomic, strong) NSMutableDictionary* objects;
+@property (nonatomic, strong) NSMutableArray* objects;
 @property (nonatomic, strong) NSString *contactID;
+@property (weak, nonatomic) IBOutlet UIButton *wishListButton;
 @end
 
 @implementation DetailProfileViewController
@@ -22,7 +23,23 @@
 {
     self = [super init];
     if(self) {
-        self.objects = (NSMutableDictionary*)[[[ProfileDetailsRequest alloc] init] getProfileWithToken:[AppDelegate getToken] andID:contactID];
+        NSDictionary *dict = [[[ProfileDetailsRequest alloc] init] getProfileWithToken:[AppDelegate getToken] andID:contactID];
+        
+        NSMutableDictionary *profile = [NSMutableDictionary dictionaryWithDictionary:[dict objectForKey:@"profile"]];
+        [profile removeObjectForKey:@"uid"];
+        
+        NSDictionary *location = [NSDictionary dictionaryWithDictionary:[dict objectForKey:@"location"]];
+        NSDictionary *other = [NSDictionary dictionaryWithDictionary:[dict objectForKey:@"other"]];
+        NSArray *skills = [NSArray arrayWithArray:[dict objectForKey:@"skills"]];
+        NSArray *needs = [NSArray arrayWithArray:[dict objectForKey:@"needs"]];
+        NSDictionary *mediaLinks = [NSDictionary dictionaryWithDictionary:[dict objectForKey:@"socialMediaLinks"]];
+        NSArray *testimonials = [NSArray arrayWithArray:[dict objectForKey:@"testimonials"]];        
+        BOOL isOnWishlist = [[dict objectForKey:@"onWishList"] boolValue];
+        if(isOnWishlist)
+            self.wishListButton.selected = YES;
+            
+        
+        self.objects = (NSMutableArray*)@[profile, location, other, skills, needs, testimonials, mediaLinks];
         self.contactID = contactID;
     }
     return self;
@@ -43,18 +60,43 @@
 
 - (NSInteger)tableView:(UITableView*)tableView numberOfRowsInSection:(NSInteger)section
 {
-    NSArray *keys = [self.objects allValues];
-    NSArray *subsec = keys[section];
-    if([subsec count] > 0)
-        return [subsec count];
-    else
-        return 1;
+    NSInteger number = [(NSArray*)self.objects[section] count];
+    if(number == 0 && section >= 3)
+        number++;
+    else if (section == 2 && number == 0) {
+        [AppDelegate showNoConnectionMessage];
+    }
+    return number;
 }
 
 - (NSString*)tableView:(UITableView*)tableView titleForHeaderInSection:(NSInteger)section
 {
-    NSArray *keys = [self.objects allKeys];
-    return keys[section];
+    switch (section) {
+        case 0:
+            return @"Profile";
+            break;
+        case 1:
+            return @"Address";
+            break;
+        case 2:
+            return @"Other";
+            break;
+        case 3:
+            return @"Skills";
+            break;
+        case 4:
+            return @"Needs";
+            break;
+        case 5:
+            return @"Testimonials";
+            break;
+        case 6:
+            return @"Social";
+            break;
+        default:
+            return @"";
+            break;
+    }
 }
 
 - (UITableViewCell*)tableView:(UITableView*)tableView cellForRowAtIndexPath:(NSIndexPath*)indexPath
@@ -66,42 +108,85 @@
         cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:CellIdentifier];
     }
     
-    NSArray *object = [self.objects allValues];
-    
-    if([[object[indexPath.section] class] isSubclassOfClass:[NSDictionary class]]) {
+    if(indexPath.section < 3) {
+        NSDictionary *dict = (NSDictionary*)self.objects[indexPath.section];
         
-        NSArray *keys = [object[indexPath.section] allKeys];
-        NSArray *value = [object[indexPath.section] allValues];
-        cell.textLabel.text = [keys[indexPath.row] description];
-        cell.detailTextLabel.text = [value[indexPath.row] description];        
-    }
-    else if ([[object[indexPath.section] class] isSubclassOfClass:[NSArray class]]) {
-        if([object[indexPath.section] count] > 0) {
-            cell.textLabel.text = [object[indexPath.section][indexPath.row] objectForKey:@"name"];
-            cell.detailTextLabel.text = @"";
+        NSArray *allKeys = [dict allKeys];
+        NSArray *allValues = [dict allValues];
+        
+        if([allKeys count] > indexPath.row) {
+            NSString *key = allKeys[indexPath.row];
+            NSString *keyCapitalized = [key stringByReplacingCharactersInRange:NSMakeRange(0,1) withString:[[key substringToIndex:1] capitalizedString]];
             
+            cell.textLabel.text = keyCapitalized;
+            cell.detailTextLabel.text = allValues[indexPath.row];
         }
-        else {        
+        else {
+            cell.textLabel.text = @"Error connection";
+            cell.detailTextLabel.text = @"";
+        }
+    }
+    else if(indexPath.section < 5) {
+        NSArray *array = (NSArray*)self.objects[indexPath.section];
+        if([array count] == 0) {
+            cell.textLabel.text = @"None listed";
+            cell.detailTextLabel.text = @"";
+        }
+        else {
+            cell.textLabel.text = [array[indexPath.row] objectForKey:@"name"];
+            cell.detailTextLabel.text = @"";
+        }
+    }
+    else if (indexPath.section == 6) {
+        NSDictionary *dict = (NSDictionary*)self.objects[indexPath.section];
+        if([dict count] > 0) {
+            NSArray *allKeys = [dict allKeys];
+            NSArray *allValues = [dict allValues];
+            
+            NSString *key = allKeys[indexPath.row];
+            NSString *keyCapitalized = [key stringByReplacingCharactersInRange:NSMakeRange(0,1) withString:[[key substringToIndex:1] capitalizedString]];
+            
+            cell.textLabel.text = keyCapitalized;
+            cell.detailTextLabel.text = allValues[indexPath.row];
+        }
+        else {
             cell.textLabel.text = @"None listed";
             cell.detailTextLabel.text = @"";
         }
     }
+    else {
+        NSArray *array = (NSArray*)self.objects[indexPath.section];
+        if([array count] == 0) {
+            cell.textLabel.text = @"None listed";
+            cell.detailTextLabel.text = @"";
+        }
+        else {
+            cell.textLabel.text = [array[indexPath.row] objectForKey:@"message"];
+            cell.detailTextLabel.text = @"";
+        }
+    }
     
-    cell.selectionStyle = UITableViewCellSelectionStyleNone;
     return cell;
 }
 
-- (IBAction)sendMessageButton:(UIButton *)sender
+#pragma mark - button events
+
+- (IBAction)sendMessageButton:(UIButton*)sender
 {
     //[self.navigationController pushViewController:[[MessageViewController alloc] initWithContactID:self.contactID] animated:YES];
 }
 
+- (IBAction)wishListButton:(UIButton*)sender
+{
+    self.wishListButton.selected = !self.wishListButton.selected;
+}
+
 #pragma mark - getter and setter
 
-- (NSMutableDictionary*)objects
+- (NSMutableArray*)objects
 {
     if(!_objects)
-        _objects = [[NSMutableDictionary alloc] init];
+        _objects = [[NSMutableArray alloc] init];
     return _objects;
 }
 
