3c9a337520fdf4f9105e54443e501fea94b20d07
Florian Reiss <85802@supinfo.com>
2013-08-13 17:21:48 -0700

    Setting up tests
    Creating a test project. Linking project and tests.

---
+++ letslunch_iPhone2_tests/letslunch_iPhone2_tests.h
@@ -0,0 +1,13 @@
+//
+//  letslunch_iPhone2_tests.h
+//  letslunch_iPhone2_tests
+//
+//  Created by Florian Reiss on 13/08/13.
+//  Copyright (c) 2013 Florian Reiss. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface letslunch_iPhone2_tests : SenTestCase
+
+@end
---
+++ letslunch_iPhone2_tests/letslunch_iPhone2_tests.m
@@ -0,0 +1,32 @@
+//
+//  letslunch_iPhone2_tests.m
+//  letslunch_iPhone2_tests
+//
+//  Created by Florian Reiss on 13/08/13.
+//  Copyright (c) 2013 Florian Reiss. All rights reserved.
+//
+
+#import "letslunch_iPhone2_tests.h"
+
+@implementation letslunch_iPhone2_tests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in letslunch_iPhone2_tests");
+}
+
+@end
